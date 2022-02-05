691932ef47aaa223c152435e12ffb88f0bec1b49
Vitaliy123 <cpc1251@mail.ru>
2022-02-06 02:09:52 +0300

    2. Сокобан. Тестовое задание. [#75283]

--- puzzle/src/main/java/ru/job4j/puzzle/Win.java
+++ puzzle/src/main/java/ru/job4j/puzzle/Win.java
@@ -1,8 +1,42 @@
 package ru.job4j.puzzle;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 public class Win {
     public static boolean check(int[][] board) {
-        boolean rsl = true;
-        return rsl;
+        int[] diagonalArray = new int[board.length];
+        int count = 0;
+        for (int i = 0; i < board.length; i++) {
+            if (board[i][i] == 1) {
+                diagonalArray[count] = i;
+                count++;
+            }
+        }
+        if (count == 0) {
+            return false;
+        }
+        diagonalArray = Arrays.copyOf(diagonalArray, count);
+        count = 0;
+        for (int i = 0; i < diagonalArray.length; i++) {
+            for (int j = 0; j < board[diagonalArray[i]].length; j++) {
+                if (board[diagonalArray[i]][j] == 1) {
+                    count++;
+                }
+                if (count == board[i].length) {
+                    return true;
+                }
+            }
+            count = 0;
+            for (int[] ints : board) {
+                if (ints[diagonalArray[i]] == 1) {
+                    count++;
+                }
+                if (count == board.length) {
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 }
