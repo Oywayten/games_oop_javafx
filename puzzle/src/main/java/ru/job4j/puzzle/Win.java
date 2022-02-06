28bd9cc2cd106348fc97a00bedd443b73aa54dd1
Vitaliy123 <cpc1251@mail.ru>
2022-02-06 12:07:03 +0300

    2. Сокобан. Тестовое задание. [#75283]

--- puzzle/src/main/java/ru/job4j/puzzle/Win.java
+++ puzzle/src/main/java/ru/job4j/puzzle/Win.java
@@ -1,40 +1,37 @@
 package ru.job4j.puzzle;
 
-import java.lang.reflect.Array;
-import java.util.Arrays;
-
 public class Win {
-    public static boolean check(int[][] board) {
-        int[] diagonalArray = new int[board.length];
-        int count = 0;
+    public static boolean monoHorizontal(int[][] board, int row) {
+        for (int i = 0; i < board[row].length; i++) {
+            if (board[row][i] != 1) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static boolean monoVertical(int[][] board, int column) {
+        for (int[] row : board) {
+            if (row[column] != 1) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static int[] extractDiagonal(int[][] board) {
+        int[] newArr = new int[board.length];
         for (int i = 0; i < board.length; i++) {
-            if (board[i][i] == 1) {
-                diagonalArray[count] = i;
-                count++;
-            }
+            newArr[i] = board[i][i];
         }
-        if (count == 0) {
-            return false;
-        }
-        diagonalArray = Arrays.copyOf(diagonalArray, count);
-        count = 0;
-        for (int i = 0; i < diagonalArray.length; i++) {
-            for (int j = 0; j < board[diagonalArray[i]].length; j++) {
-                if (board[diagonalArray[i]][j] == 1) {
-                    count++;
-                }
-                if (count == board[i].length) {
-                    return true;
-                }
-            }
-            count = 0;
-            for (int[] ints : board) {
-                if (ints[diagonalArray[i]] == 1) {
-                    count++;
-                }
-                if (count == board.length) {
-                    return true;
-                }
+        return newArr;
+    }
+
+    public static boolean check(int[][] board) {
+        int[] newArr = extractDiagonal(board);
+        for (int i = 0; i < newArr.length; i++) {
+            if ((newArr[i] == 1) && (monoHorizontal(board, i) || monoVertical(board, i))) {
+                return true;
             }
         }
         return false;
