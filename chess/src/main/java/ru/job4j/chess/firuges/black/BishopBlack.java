abd9dd0fb9ebb613866bab54146ec03671370c26
Vitaliy123 <cpc1251@mail.ru>
2022-03-03 21:03:31 +0300

    2. Каркас шахматной доски [#792]

--- chess/src/main/java/ru/job4j/chess/firuges/black/BishopBlack.java
+++ chess/src/main/java/ru/job4j/chess/firuges/black/BishopBlack.java
@@ -25,11 +25,13 @@ public class BishopBlack implements Figure {
             );
         }
         Cell[] steps = new Cell[size];
-        int deltaX = dest.getX() - position.getX();
-        int deltaY = dest.getY() - position.getY();
+        int deltaX = dest.getX() - position.getX() > 0 ? 1 : -1;
+        int deltaY = dest.getY() - position.getY() > 0 ? 1 : -1;
+        int x = position.getX();
+        int y = position.getY();
         for (int index = 0; index < size; index++) {
-                int x = position.getX() + deltaX / Math.abs(deltaX) * (index + 1);
-                int y = position.getY() + deltaY / Math.abs(deltaY) * (index + 1);
+                x += deltaX;
+                y += deltaY;
             steps[index] = Cell.findBy(x, y);
         }
             return steps;
@@ -37,9 +39,11 @@ public class BishopBlack implements Figure {
     }
 
     public boolean isDiagonal(Cell source, Cell dest) {
-        int deltaX = Math.abs(source.getX() - dest.getX());
-        int deltaY = Math.abs(source.getY() - dest.getY());
-        return deltaX == deltaY && deltaX != 0;
+        return Math.abs(source.getX()
+                - dest.getX())
+                == Math.abs(source.getY()
+                - dest.getY()) && source.getX()
+                - dest.getX() != 0;
     }
 
     @Override
--- chess/src/test/java/ru/job4j/chess/LogicTest.java
+++ chess/src/test/java/ru/job4j/chess/LogicTest.java
@@ -1,7 +1,5 @@
 package ru.job4j.chess;
 
-import org.junit.Assert;
-import org.junit.Ignore;
 import org.junit.Test;
 import ru.job4j.chess.firuges.Cell;
 import ru.job4j.chess.firuges.Figure;
@@ -9,7 +7,7 @@ import ru.job4j.chess.firuges.black.BishopBlack;
 
 public class LogicTest {
 
-    @Test
+    @Test(expected = FigureNotFoundException.class)
     public void whenMoveFigureNotFoundException()
             throws FigureNotFoundException, ImpossibleMoveException, OccupiedCellException {
         Logic logic = new Logic();
@@ -19,19 +17,13 @@ public class LogicTest {
         figures[2] = new BishopBlack(Cell.A3);
         figures[3] = new BishopBlack(Cell.A4);
         figures[4] = new BishopBlack(Cell.A5);
-        for (int i = 0; i < figures.length; i++) {
-            logic.add(figures[i]);
+        for (Figure figure : figures) {
+            logic.add(figure);
         }
-        try {
             logic.move(Cell.A6, Cell.A7);
-        } catch (FigureNotFoundException e) {
-            Assert.assertEquals(
-                    "ru.job4j.chess.FigureNotFoundException",
-                    e.toString());
-        }
     }
 
-    @Test
+    @Test(expected = ImpossibleMoveException.class)
     public void whenMoveImpossibleMoveException()
             throws FigureNotFoundException, ImpossibleMoveException, OccupiedCellException {
         Logic logic = new Logic();
@@ -41,20 +33,13 @@ public class LogicTest {
         figures[2] = new BishopBlack(Cell.A3);
         figures[3] = new BishopBlack(Cell.A4);
         figures[4] = new BishopBlack(Cell.A5);
-        for (int i = 0; i < figures.length; i++) {
-            logic.add(figures[i]);
+        for (Figure figure : figures) {
+            logic.add(figure);
         }
-        try {
             logic.move(Cell.A1, Cell.B1);
-        } catch (ImpossibleMoveException e) {
-            Assert.assertEquals(
-                    "ru.job4j.chess.ImpossibleMoveException: "
-                            + "Could not way by diagonal from A1 to B1",
-                    e.toString());
-        }
     }
 
-    @Test
+    @Test(expected = OccupiedCellException.class)
     public void whenMoveOccupiedCellException()
             throws FigureNotFoundException, ImpossibleMoveException, OccupiedCellException {
         Logic logic = new Logic();
@@ -64,15 +49,9 @@ public class LogicTest {
         figures[2] = new BishopBlack(Cell.A3);
         figures[3] = new BishopBlack(Cell.A4);
         figures[4] = new BishopBlack(Cell.C3);
-        for (int i = 0; i < figures.length; i++) {
-            logic.add(figures[i]);
+        for (Figure figure : figures) {
+            logic.add(figure);
         }
-        try {
             logic.move(Cell.A1, Cell.C3);
-        } catch (OccupiedCellException e) {
-            Assert.assertEquals(
-                    "ru.job4j.chess.OccupiedCellException",
-                    e.toString());
-        }
     }
 }
